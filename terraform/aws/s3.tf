20d9e945d470a8dbba82a1994f9aeb01361c86d5
bmppa <bm.almeida@gmail.com>
2021-10-05 10:56:13 -0400

    changed bucket name

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "data" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
-  bucket        = "my-bucket-8357"
+  bucket        = "my-bucket-83579"
   acl           = "public-read"
   force_destroy = true
 }
