b753af65c4e0200f994a980a2f9d87dce8c56331
bmppa <65456094+bmppa@users.noreply.github.com>
2022-03-07 11:04:41 -0500

    Update main.tf

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -1,9 +1,16 @@
 terraform {
   backend "remote" {
-    organization = "pcs-nym"
+    organization = "balmeida"
 
     workspaces {
       name = "iac-demo"
     }
   }
+}
+
+resource "google_storage_bucket" "my-bucket" {
+  name = "my-bucket"
+  location = us-central1
+  force_destroy = true
+  uniform_bucket_level_access = true
 }
