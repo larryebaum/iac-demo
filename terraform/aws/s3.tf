bf2636cfe85a20de4ac7fae4200362ebdc572688
bmppa <65456094+bmppa@users.noreply.github.com>
2022-04-22 09:23:12 -0400

    Removed Yor tags

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -6,14 +6,4 @@ resource "aws_s3_bucket" "data" {
   bucket        = "my-bucket-835799"
   acl           = "public-read"
   force_destroy = true
-  tags = {
-    git_commit           = "7615def883560bd6169d34d3cbe017b3e2a4cce7"
-    git_file             = "terraform/aws/s3.tf"
-    git_last_modified_at = "2021-10-05 15:00:19"
-    git_last_modified_by = "bm.almeida@gmail.com"
-    git_modifiers        = "bm.almeida"
-    git_org              = "bmppa"
-    git_repo             = "iac-demo"
-    yor_trace            = "8cebc6f3-14b0-4a60-bd65-7988a2c206f1"
-  }
 }
