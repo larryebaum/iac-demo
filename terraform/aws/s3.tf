cc905a62b471b82cecec0a4f43d89257dbad6457
bralmeidapan <bralmeida@paloaltonetworks.com>
2023-03-08 15:00:01 -0500

    fixed S3 block

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -3,6 +3,7 @@ resource "aws_s3_bucket" "public_bucket" {
   bucket = "my-public-bucket-${random_string.random.result}"
 
   tags = {
-    Privacy = "public"
-    Proj    = "prisma-cloud-pov"
+    Privacy   = "public"
+    Proj      = "prisma-cloud-pov"
   }
+}
