1670341c0841ca8c51d2df78f1763df0bd7aa8e2
bmppa <65456094+bmppa@users.noreply.github.com>
2023-03-10 00:01:47 -0500

    Create s3.tf

---
+++ terraform/aws/s3.tf
@@ -0,0 +1,9 @@
+# Create a bucket where objects can be public
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "my-public-bucket-${random_string.random.result}"
+
+  tags = {
+    Privacy   = "public"
+    Proj      = "prisma-cloud-pov"
+  }
+}
